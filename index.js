89c94746928497deb39aade186225753ef9c64e0
Jewel Carrier-Davis <jujicb@gmail.com>
2022-07-01 19:37:40 -0400

    commit tree 833827ab0b7077694609669a910815078778c416

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin (record) {
+    const result = record.find(game => game.result === 'W')
+
+    if (result === undefined) {
+        return undefined
+    }
+    else {
+        return result.year
+    }
+}
+
+superbowlWin(record);
